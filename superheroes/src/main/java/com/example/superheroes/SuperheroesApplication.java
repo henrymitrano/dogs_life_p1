8e8bd77fe61de7e3585e19aa9bd96f43f7ea0d3f
Meg <mpenaganti12@gmail.com>
2023-07-28 21:14:23 +0100

    Adding controller

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -12,22 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
+public class SuperheroesApplication {
 
 	@Autowired
 	HeroService heroService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		List<Hero> heroes = heroService.getLastThreeHeroesByName();
-		for (Hero hero : heroes) {
-			System.out.println(hero);
-		}
-
-
 	}
 }
