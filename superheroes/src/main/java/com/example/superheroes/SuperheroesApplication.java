1292981af9c1f3b26d64beded94c9f5a0fa064f1
Meg <mpenaganti12@gmail.com>
2023-07-28 20:33:46 +0100

    Adding service layer

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -3,6 +3,7 @@ package com.example.superheroes;
 import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
 import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +15,7 @@ import java.util.List;
 public class SuperheroesApplication implements CommandLineRunner {
 
 	@Autowired
-	HeroRepository heroRepository;
+	HeroService heroService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -22,7 +23,7 @@ public class SuperheroesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("C%");
+		List<Hero> heroes = heroService.getLastThreeHeroesByName();
 		for (Hero hero : heroes) {
 			System.out.println(hero);
 		}
