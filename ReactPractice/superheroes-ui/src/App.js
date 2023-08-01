68305657008d3fbfaa8bc849fcdb5b684bc2cfd5
Meg <mpenaganti12@gmail.com>
2023-08-01 14:42:31 +0100

    adding frontend functionality

--- ReactPractice/superheroes-ui/src/App.js
+++ ReactPractice/superheroes-ui/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import HeroDetail from './components/HeroDetail';
+import AllHeroes from './components/AllHeroes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <HeroDetail/>
+    <AllHeroes/>
   );
 }
 
